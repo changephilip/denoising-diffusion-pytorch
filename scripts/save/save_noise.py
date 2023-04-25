0cfc70e998763653e6615f565d6ad0d602142397
changephilip <pcchange90@outlook.com>
2023-04-25 12:47:41 +0800

    update AWGN

--- scripts/save/save_noise.py
+++ scripts/save/save_noise.py
@@ -13,7 +13,10 @@ import tqdm
 import cv2
 
 test_path = "../../test"
-
+num_pdb = 1
+num_projection = 1
+num_pic = -1
+snr_array=[-8,-4,-2, -1,0,1,2,4,8]
 
 def awgnr(s:np.array, snr:int):
     """
@@ -22,12 +25,12 @@ def awgnr(s:np.array, snr:int):
     """
     # power of signal
     nzs = np.count_nonzero(s)
-    nzs = s.size
+    #nzs = s.size
 
     print(nzs)
     Ps = np.sum(np.power(s,2))/nzs
     print(Ps)
-    Pn = Ps * (np.power(2, snr))
+    Pn = Ps * (2**snr)
     print(Pn)
     noise = np.random.randn(s.size).reshape(s.shape) * np.sqrt(Pn)
     sn = s + noise
@@ -54,22 +57,23 @@ def awgnc(s:np.array, snr:int):
 
     return sn
 
-pdblist=linecache.getlines(test_path+'/cas9/list.txt')[0].split('\n')[0].split(',')[0:1]
+pdblist=linecache.getlines(test_path+'/cas9/list.txt')[0].split('\n')[0].split(',')[0:num_pdb]
 
 img_list=[]
 for pdb in tqdm.tqdm(pdblist):
     f=mrcfile.open(test_path+'/cas9_volume/'+pdb+'.mrc')
-    for img in f.data[0:2]:
-        img=scipy.ndimage.zoom(img,64/320)
-        #img_n = awgnr(img,1)
-        img_=(img-np.min(img))/(np.max(img)-np.min(img))
-        img_n = awgnr(img_,4)
-        img_list.append(img_n.reshape(1,64,64))
+    for img in f.data[0:num_projection]:
+        #img=scipy.ndimage.zoom(img,64/320)
+        for snr in snr_array:
+            #img_n = awgnr(img,snr)
+            img_=(img-np.min(img))/(np.max(img)-np.min(img))
+            img_n = awgnr(img_,snr)
+            img_list.append(img_n.reshape(1,320,320))
     f.close()
 np_list=np.array(img_list)
 t_list=torch.tensor(np_list)
 #for i in tqdm.tqdm(range(0,len(img_list))):
-for i in tqdm.tqdm(range(0,2)):
-    #torchvision.utils.save_image(t_list[i],str(i)+".pgm")
+i=0
+for im in tqdm.tqdm(np_list[0:num_pic]):
     cv2.imwrite(test_path+"/pic_noise/"+str(i)+".png",(128 - np_list[i][0]*128).astype('uint8'))
-    #cv2.imwrite(test_path+"/pic_noise/"+str(i)+".tiff",- np_list[i][0])
+    i=i+1
